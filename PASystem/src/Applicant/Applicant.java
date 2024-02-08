9a07c0c7751b1db2f28661323a0e43b4c4138dc8
Vidun Nethdina <vinethdina77@gmail.com>
2024-02-08 06:43:24 +0530

    railway

--- PASystem/src/Applicant/Applicant.java
+++ PASystem/src/Applicant/Applicant.java
@@ -14,4 +14,48 @@ public class Applicant {
 	//private Address address;
 	
 	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getPob() {
+		return pob;
+	}
+	public void setPob(String pob) {
+		this.pob = pob;
+	}
+	public String getFatherName() {
+		return fatherName;
+	}
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	public String getMotherName() {
+		return motherName;
+	}
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+	
+	
 }
--- PASystem/src/Applicant/ApplicantController.java
+++ PASystem/src/Applicant/ApplicantController.java
@@ -1,5 +1,52 @@
 package Applicant;
 
+import java.sql.Date;
+
 public class ApplicantController {
+	private Applicant model;
+	private ApplicantView view;
 	
+	public ApplicantController(Applicant model, ApplicantView view) {
+		this.model= model;
+		this.view = view;
+	}
+	public void setApplicantName(String firstName, String lastName){
+		model.setFirstName(firstName);
+		model.setLastName(lastName);
+	}
+	public void setApplicantAge(int age) {
+		model.setAge(age);
+	}
+	public void setApplicantDob(Date date) {
+		model.setDob(date);
+	}
+	public void setApplicantPob(String pob) {
+		model.setPob(pob);
+	}
+	public void setApplicantParents(String fatherName, String motherName) {
+		model.setFatherName(fatherName);
+		model.setMotherName(motherName);
+	}
+	//getters
+	public String getApplicantFirstName() {
+		return model.getFirstName();
+	}
+	public String getApplicantLastName() {
+		return model.getLastName();
+	}
+	public int getApplicantAge() {
+		return model.getAge();
+	}
+	public Date getApplicantDob() {
+		return model.getDob();
+	}
+	public String getApplicantPob() {
+		return model.getPob();
+	}
+	public String getApplicantFather() {
+		return model.getFatherName();
+	}
+	public String getApplicantMother() {
+		return model.getMotherName();	}
+
 }
